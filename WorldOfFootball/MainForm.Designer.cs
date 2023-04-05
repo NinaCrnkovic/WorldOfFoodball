58aa96e099808b4c2ac272cd34c156b67db3a298
Nina Crnković <nina.crnkovic.zg@gmail.com>
2023-04-05 22:53:54 +0200

    Changed name, added some styling

--- WorldOfFootball/MainForm.Designer.cs
+++ WorldOfFootball/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WorldOfFoodbal
+﻿namespace WorldOfFootball
 {
     partial class MainForm
     {
@@ -28,21 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbTeams = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbTeams = new System.Windows.Forms.ListBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lbTeams
-            // 
-            this.lbTeams.FormattingEnabled = true;
-            this.lbTeams.ItemHeight = 20;
-            this.lbTeams.Location = new System.Drawing.Point(593, 377);
-            this.lbTeams.Name = "lbTeams";
-            this.lbTeams.Size = new System.Drawing.Size(264, 224);
-            this.lbTeams.TabIndex = 1;
             // 
             // panel1
             // 
@@ -52,7 +43,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1382, 125);
-            this.panel1.TabIndex = 2;
+            this.panel1.TabIndex = 3;
             // 
             // pictureBox1
             // 
@@ -62,15 +53,24 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // lbTeams
+            // 
+            this.lbTeams.FormattingEnabled = true;
+            this.lbTeams.ItemHeight = 20;
+            this.lbTeams.Location = new System.Drawing.Point(559, 264);
+            this.lbTeams.Name = "lbTeams";
+            this.lbTeams.Size = new System.Drawing.Size(264, 224);
+            this.lbTeams.TabIndex = 4;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1382, 753);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.lbTeams);
+            this.Controls.Add(this.panel1);
             this.Name = "MainForm";
-            this.Text = "World of Foodball";
+            this.Text = "World of Football";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -79,8 +79,9 @@
         }
 
         #endregion
-        private ListBox lbTeams;
+
         private Panel panel1;
         private PictureBox pictureBox1;
+        private ListBox lbTeams;
     }
 }
