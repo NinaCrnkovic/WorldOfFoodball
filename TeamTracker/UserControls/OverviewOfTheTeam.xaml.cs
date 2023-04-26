148c119d73556ca1914f47afd00a8a6e73bf8cd7
Nina Crnković <nina.crnkovic.zg@gmail.com>
2023-04-26 16:57:34 +0200

    Added overview user control

---
+++ TeamTracker/UserControls/OverviewOfTheTeam.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeamTracker.UserControls
+{
+    /// <summary>
+    /// Interaction logic for OverviewOfTheTeam.xaml
+    /// </summary>
+    public partial class OverviewOfTheTeam : UserControl
+    {
+        public OverviewOfTheTeam()
+        {
+            InitializeComponent();
+        }
+    }
+}
